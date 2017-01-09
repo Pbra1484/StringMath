a559d71be30379d4f02266c801aaa261c286207d
Pbra1484 <pbra1484@740s-pl-123.local>
2017-01-09 13:55:02 -0700

    added dtamebers and logic

--- src/string/controller/StringController.java
+++ src/string/controller/StringController.java
@@ -2,6 +2,24 @@ package string.controller;
 
 public class StringController 
 {
+	
+	
+	
+	String number1;
+	String number2;
+	int cNumber;
+	int carry;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	StringController()
 	{
 		
@@ -10,7 +28,30 @@ public class StringController
 	
 	public void start()
 	{
+		int length;
+		if(number1.length() > number2.length())
+		{
+			length = number1.length();
+		}
+		else
+		{
+			String temp = number1;
+			number1 = number2;
+			number2 = temp;
+			length = number1.length();
+		}
 		
+		for(int index = 0; index < length; index++)
+		{
+			
+			
+		}
+		
+	}
+	
+	public  void print(String print)
+	{
+		System.out.println(print);
 	}
 
 }
